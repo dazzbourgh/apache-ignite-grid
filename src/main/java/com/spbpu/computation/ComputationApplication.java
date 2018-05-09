1e25370ab1fa2809abd05ede0f6595926f69fc12
dazzbourgh <dazzbourgh@gmail.com>
2018-05-09 12:51:26 +0300

    calculates fibonacci on a single local cluster

--- src/main/java/com/spbpu/computation/ComputationApplication.java
+++ src/main/java/com/spbpu/computation/ComputationApplication.java
@@ -1,25 +1,46 @@
 package com.spbpu.computation;
 
+import com.spbpu.computation.beans.ClosureFactory;
 import com.spbpu.computation.beans.IgniteFactory;
+import com.spbpu.computation.closure.ContinuationFibonacciClosure;
 import org.apache.ignite.Ignite;
+import org.apache.ignite.cluster.ClusterNode;
+import org.apache.ignite.lang.IgnitePredicate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.math.BigInteger;
+import java.util.UUID;
+
 @SpringBootApplication
 public class ComputationApplication implements CommandLineRunner {
     @Autowired
     private IgniteFactory igniteFactory;
+    @Autowired
+    private ClosureFactory closureFactory;
 
     public static void main(String[] args) {
         SpringApplication.run(ComputationApplication.class, args);
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         try (Ignite ignite = igniteFactory.ignite()) {
-            System.out.println("works");
+            long N = 8;
+            final UUID exampleNodeId = ignite.cluster()
+                    .localNode()
+                    .id();
+            IgnitePredicate<ClusterNode> nodeFilter = n -> ignite.cluster()
+                    .forRemotes()
+                    .nodes()
+                    .isEmpty() || !n.id().equals(exampleNodeId);
+            ContinuationFibonacciClosure closure = closureFactory
+                    .fibonacci(nodeFilter);
+            BigInteger result = ignite.compute(ignite.cluster().forPredicate(nodeFilter))
+                    .apply(closure, N);
+            System.out.println("The result is: " + result);
         }
     }
 }
