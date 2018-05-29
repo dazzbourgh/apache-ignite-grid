47cec27b7eb7dda0d34cd7d3fd84dae89f81aaf4
PETERSBURG\Leonid_Borisevich <leonid_borisevich@epam.com>
2018-05-29 23:05:24 +0300

    dependencies from example, separate configs + security

--- src/main/java/com/spbpu/computation/ComputationApplication.java
+++ src/main/java/com/spbpu/computation/ComputationApplication.java
@@ -1,31 +1,15 @@
 package com.spbpu.computation;
 
-import com.spbpu.computation.beans.IgniteFactory;
 import org.apache.ignite.Ignite;
+import org.apache.ignite.Ignition;
 import org.apache.ignite.cluster.ClusterGroup;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
-import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Collection;
 
-@SpringBootApplication
-@ImportResource("classpath:config.xml")
-public class ComputationApplication implements CommandLineRunner {
-    @Autowired
-    private IgniteFactory igniteFactory;
-
+public class ComputationApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ComputationApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) {
-        try (Ignite ignite = igniteFactory.ignite()) {
+        try (Ignite ignite = Ignition.start("config/client.xml")) {
             ClusterGroup clusterGroup = ignite.cluster().forServers();
             Collection<Integer> result = ignite.compute(clusterGroup)
                     .apply((String line) -> {
