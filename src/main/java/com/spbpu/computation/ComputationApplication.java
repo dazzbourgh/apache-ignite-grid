9a9deb35891245cc717e1862cabcb1db1a0d283b
PETERSBURG\Leonid_Borisevich <leonid_borisevich@epam.com>
2018-05-30 22:31:56 +0300

    java 7 and more config tweaking

--- src/main/java/com/spbpu/computation/ComputationApplication.java
+++ src/main/java/com/spbpu/computation/ComputationApplication.java
@@ -3,6 +3,7 @@ package com.spbpu.computation;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.cluster.ClusterGroup;
+import org.apache.ignite.lang.IgniteClosure;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -10,18 +11,27 @@ import java.util.Collection;
 public class ComputationApplication {
     public static void main(String[] args) {
         try (Ignite ignite = Ignition.start("config/client.xml")) {
-            ClusterGroup clusterGroup = ignite.cluster().forServers();
+            ignite.configuration().setClientMode(true);
+            ClusterGroup clusterGroup = ignite.cluster().forRemotes();
             Collection<Integer> result = ignite.compute(clusterGroup)
-                    .apply((String line) -> {
-                                System.out.println("Computing for line:\n\t" + line);
-                                return Arrays.stream(line.split(" "))
-                                        .mapToInt(word -> 1)
-                                        .sum();
-                            },
+                    .apply(new IgniteClosure<String, Integer>() {
+                               @Override
+                               public Integer apply(String line) {
+                                   System.out.println("Computing for line:\n\t" + line);
+                                   String[] words = line.split(" ");
+                                   int count = 0;
+                                   for (String word : words) {
+                                       ++count;
+                                   }
+                                   return count;
+                               }
+                           },
                             Arrays.asList("Some text\nThat takes\nSeveral lines".split("\n")));
-            System.out.println("The result is: " + result.stream()
-                    .mapToInt(it -> it)
-                    .sum());
+            int resultNumber = 0;
+            for (Integer i : result) {
+                resultNumber += i;
+            }
+            System.out.println("The result is: " + resultNumber);
         }
     }
 }
